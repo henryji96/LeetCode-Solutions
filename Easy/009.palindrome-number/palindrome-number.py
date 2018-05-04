51b063f405c7f4380836e3775b2201a825b59ce3
Wenhan Ji <jiwenhan@foxmail.com>
2018-05-05 01:33:50 +0800

    Update 009,013 Solutions

--- Easy/009.palindrome-number/palindrome-number.py
+++ Easy/009.palindrome-number/palindrome-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isPalindrome(self, x):
+    def isPalindrome1(self, x):
         """
         :type x: int
         :rtype: bool
@@ -12,3 +12,17 @@ class Solution:
             return True
         else:
             return False
+        
+    def isPalindrome2(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        half = 0
+        while x > half:
+            half = half * 10 + x % 10
+            x /= 10
+        return x == half or half / 10 == x
+
