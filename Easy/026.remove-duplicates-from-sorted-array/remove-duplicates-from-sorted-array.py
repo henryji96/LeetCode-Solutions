ac51e01f4d721f730e5cee253d63de40f1efa556
Wenhan Ji <jiwenhan@foxmail.com>
2018-05-06 13:57:31 +0800

    update 026

--- Easy/026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ Easy/026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -12,3 +12,17 @@ class Solution:
             nums.remove(val)
                 
         return len(nums)
+    
+    def removeDuplicates2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
+        slow = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[i]
+        return slow + 1
