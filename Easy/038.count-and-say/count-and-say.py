3efb1109036262337b235b6515076282189ae0de
Wenhan Ji <jiwenhan@foxmail.com>
2018-05-08 16:18:25 +0800

    ac 038

---
+++ Easy/038.count-and-say/count-and-say.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        ans = "1"
+        for i in range(1,n):
+            ans = self.sayNum(ans)
+        return ans
+            
+    
+    def sayNum(self, nums):
+        
+        
+        numsStr = str(nums)
+        cnt = 1
+        prevNum = numsStr[0]
+        ans = ""
+        
+        if(len(numsStr) == 1):
+            return "1" + numsStr
+        
+        for i in range(1, len(numsStr)):
+            if(numsStr[i] == numsStr[i-1]):
+                cnt = cnt + 1
+            else:
+                ans = ans + str(cnt) + numsStr[i-1]
+                cnt = 1
+            if(i == len(numsStr)-1):
+                ans = ans + str(cnt) + numsStr[i] 
+        return ans
+    
+           
