7c16324d6199d3036b31a5f1e4cf13ad5190c580
Wenhan Ji <jiwenhan@foxmail.com>
2018-06-11 15:50:42 +0800

    ac 069 binary search

--- Easy/069.sqrt(x)/sqrt(x).py
+++ Easy/069.sqrt(x)/sqrt(x).py
@@ -1,8 +1,18 @@
-import math
 class Solution:
     def mySqrt(self, x):
         """
         :type x: int
         :rtype: int
         """
-        return int(math.sqrt(x))
+        low = 1
+        high = x
+        while low <= high:
+            middle = low + (high - low) // 2
+            if middle*middle > x:
+                high = middle - 1
+            else:
+                low = middle + 1
+        return high
+if __name__ == '__main__':
+    s = Solution()
+    print(s.mySqrt(8))
