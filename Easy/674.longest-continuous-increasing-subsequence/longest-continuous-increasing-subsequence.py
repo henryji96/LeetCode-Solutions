faeaa9c32b0609822236d5688a738e29f31cd211
Wenhan Ji <jiwenhan@foxmail.com>
2018-06-15 09:36:01 +0800

    update 674 dp

--- Easy/674.longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.py
+++ Easy/674.longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.py
@@ -1,1 +1,19 @@
-longest-continuous-increasing-subsequence
+class Solution:
+    def findLengthOfLCIS(self, nums):
+        """
+            :type nums: List[int]
+            :rtype: int
+            """
+        if not nums:
+            return 0
+        
+        ans = 1
+        currentMax = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                currentMax += 1
+            else:
+                currentMax = 1
+            
+            ans = max(currentMax, ans)
+        return ans
