13fbb77a14666f81adc54845e91fe6d2b2669da8
Wenhan Ji <jiwenhan@foxmail.com>
2018-06-24 00:51:57 +0800

    ac 792

--- Medium/792.number-of-matching-subsequences/number-of-matching-subsequences.py
+++ Medium/792.number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -16,3 +16,39 @@ class Solution(object):
 
 
         return sum(isMatch(S, word) for word in words)
+
+##############################################################
+import bisect
+class Solution2(object):
+    def numMatchingSubseq(self, S, words):
+        """
+        :type S: str
+        :type words: List[str]
+        :rtype: int
+        """
+
+        sCharPos = {}
+        for i in range(len(S)):
+            if S[i] not in sCharPos.keys():
+                sCharPos[S[i]] = [i]
+            else:
+                sCharPos[S[i]].append(i)
+
+        return sum(self.isMatch(sCharPos, word) for word in words)
+
+
+    def isMatch(self, sCharPos, word):
+
+        nextStart = 0
+        for c in word:
+            if c in sCharPos:
+                cIndexList = sCharPos[c]
+                i = bisect.bisect_right(cIndexList, nextStart)
+
+                if i < len(cIndexList):
+                    nextStart = cIndexList[i]
+                else:
+                    return False
+            else:
+                return False
+        return True
