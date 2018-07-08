b3957f5c898f7a94f6c19b56f469925e36ec7dbb
Wenhan Ji <jiwenhan@foxmail.com>
2018-07-08 23:25:29 +0800

    ac 826 greedy

--- Medium/826.most-profit-assigning-work/most-profit-assigning-work.py
+++ Medium/826.most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,1 +1,24 @@
-most-profit-assigning-work
+import bisect
+class Solution:
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        """
+        :type difficulty: List[int]
+        :type profit: List[int]
+        :type worker: List[int]
+        :rtype: int
+        """
+        difficulty_profit = []
+        for i in range(len(profit)):
+            difficulty_profit.append(difficulty[i], profit[i])
+        difficulty_profit = sorted(difficulty_profit, key = lambda x: x[0])
+
+        ans = 0
+        for workerAbility in worker:
+            i = 0
+            maxProfit = 0
+            while workerAbility <= difficulty_profit[i][0]:
+
+                maxProfit = max(maxProfit, difficulty_profit[i][1])
+                i += 1
+            ans += maxProfit
+        return ans
