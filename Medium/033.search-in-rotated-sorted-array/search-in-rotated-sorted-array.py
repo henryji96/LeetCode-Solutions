b5ce623294edeb71fa543af09fdd966d4a6423cf
Wenhan Ji <jiwenhan@foxmail.com>
2018-07-19 13:22:33 +0800

    ac 033 binary search

---
+++ Medium/033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if nums == []:
+            return -1
+
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+
+            mid = low + (high - low) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
