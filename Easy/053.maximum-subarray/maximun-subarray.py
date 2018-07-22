5b856b5375d3c1003704d221ab4c6da92c6c7835
Wenhan Ji <jiwenhan@foxmail.com>
2018-07-22 20:18:33 +0800

    modify 053

--- Easy/053.maximum-subarray/maximun-subarray.py
+++ Easy/053.maximum-subarray/maximun-subarray.py
@@ -1,3 +1,6 @@
+# nums   [-1, 2, -2, 4, -8, 5, -9, 6]
+# curSum [-1, 3,  1, 5,  0, 5,  0, 6]
+
 class Solution:
     def maxSubArray(self, nums):
         """
@@ -6,10 +9,12 @@ class Solution:
         """
         curSum = 0
         maxSum = nums[0]
-        
+
         for num in nums:
             if(curSum <= 0):
                 curSum = 0
+
             curSum = curSum + num
+
             maxSum = max(curSum, maxSum)
         return maxSum
