03ccce65d819979392f9a9a6a40c8e9febff9387
Wenhan Ji <jiwenhan@foxmail.com>
2018-07-23 14:37:32 +0800

    ac 342

---
+++ Easy/342.power-of-four/power-of-four.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+
+        while num % 4 == 0:
+            num /= 4
+
+        return num == 1
