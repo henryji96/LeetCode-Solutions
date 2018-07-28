e143c269228593a066374efbe0fab29043c4ab31
Wenhan Ji <jiwenhan@foxmail.com>
2018-07-28 23:44:54 +0800

    ac 151 string

---
+++ Medium/151.reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        sl = s.split(" ")[::-1]
+
+        ans = ""
+        for word in sl:
+            if word != "":
+                ans = ans + word + " "
+        return ans[:-1]
