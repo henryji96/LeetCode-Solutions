8e12102c3fec1a313b6fd60c9b1191150aeed197
Wenhan Ji <jiwenhan@foxmail.com>
2018-07-29 17:21:27 +0800

    ac 055 greedy

---
+++ Medium/055.jump-game/jump-game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        m = 0
+
+        for i in range(len(nums)):
+            if i > m:
+                return False
+            m = max(m, i + nums[i])
+        return True
